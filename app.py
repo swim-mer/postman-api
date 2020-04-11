d42de1844734a911276f91e093e718ced16defb2
Kelly Hodis <kelly.hodis@processmaker.com>
2020-04-10 17:07:00 -0700

    Add auth module

--- app.py
+++ app.py
@@ -2,11 +2,15 @@
 """
 Start a Flask web app.
 """
-from flask import Flask
+from flask import Flask, make_response
+from flask_httpauth import HTTPBasicAuth
+
 
 # Web app to run
 app = Flask(__name__)
 
+# auth object
+auth = HTTPBasicAuth()
 
 # Memory database
 title = [
@@ -19,9 +23,24 @@ title = [
 def home():
     return "Home Page"
 
+
 @app.route('/login', strict_slashes=False)
+@auth.login_required
 def login():
     return "Login"
+
+
+# Authentication
+@auth.get_password
+def get_password(username):
+    if username == 'admin':
+        return 'admin'
+    return None
+
+
+@auth.error_handler
+def unauthorized():
+    return make_response({'Error': 'Invalid credentials'}, 404)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='8888')
